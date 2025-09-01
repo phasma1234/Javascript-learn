62a7a3754170d2b5d183e24e67656b23b3bee42d
phasma <titolinti@gmail.com>
2025-09-01 05:38:10 +0300

    another Javascript Object lesson

--- objects.js
+++ objects.js
@@ -6,3 +6,13 @@ var person={
 };
 
 document.getElementById("azaboi").innerHTML=person.firstName + person.lastName + " is" + person.age +" and has " + person.eyeColor +" eye color";
+
+const car={
+    brand: " BMW",
+    color: "Red ",
+    yearOfProduction:2025,
+    country:"German"
+};
+
+document.getElementById('mchumba').innerHTML= "Hamisi owns a "+car.color + " " + car.brand + ' from '+car.country + 
+    " that has been manufactured on "+car.yearOfProduction;
